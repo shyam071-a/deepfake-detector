4c2b6986079a2cbdac37a565e37a2c4663e12827
shyam071-a <shyammohanmaurya723405@gmail.com>
2026-03-31 16:35:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
 from flask import Flask, request, jsonify
-import cv2, numpy as np, os
-from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
-model = load_model("model.h5")
 
-def preprocess(img):
-    img = cv2.resize(img, (224,224))
-    img = img/255.0
-    return np.reshape(img,(1,224,224,3))
+@app.route("/")
+def home():
+    return jsonify({"message": "API Working 🚀"})
 
 @app.route("/detect", methods=["POST"])
 def detect():
-    file = request.files["file"]
-    path = "temp.jpg"
-    file.save(path)
-
-    img = cv2.imread(path)
-    img = preprocess(img)
-    pred = model.predict(img)[0][0]
-
-    os.remove(path)
-
-    if pred > 0.5:
-        return jsonify({"result":"FAKE","confidence":float(pred*100)})
-    else:
-        return jsonify({"result":"REAL","confidence":float((1-pred)*100)})
+    # temporary response (testing ke liye)
+    return jsonify({
+        "result": "FAKE",
+        "confidence": 95
+    })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
