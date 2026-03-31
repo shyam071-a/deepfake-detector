3c315ef3736c94b8f162d327bda60449e17107d8
shyam071-a <shyammohanmaurya723405@gmail.com>
2026-03-31 19:25:02 +0530

    Update logging and response comments in detect function

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,9 @@ def home():
 @app.route("/detect", methods=["POST"])
 def detect():
     try:
+        print("API HIT 🔥")
+
+        # check file
         if "file" not in request.files:
             return jsonify({"error": "No file uploaded"}), 400
 
@@ -19,13 +22,14 @@ def detect():
         if file.filename == "":
             return jsonify({"error": "Empty file"}), 400
 
-        # Dummy response (AI later)
+        # TEMP response (AI later)
         return jsonify({
             "result": "FAKE",
             "confidence": 92
         })
 
     except Exception as e:
+        print("ERROR:", str(e))
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
